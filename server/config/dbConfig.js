41338250e2a447b23a5227f8dd457fb8a6e64aee
Nata J. Negara <pranatajayanegara@gmail.com>
2025-10-20 17:30:16 +0700

    add proper readme

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnet = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "mern-reminder",
+      dbName: process.env.DB_NAME,
     });
     console.log(`db connected successfully: ${conn.connection.host}`);
   } catch (error) {
